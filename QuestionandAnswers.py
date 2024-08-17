a4070dd42f72cf04c68f5258cd9bce8137ef4032
Anshul Kumar <Sonakiyaanshul1127gmblast@gmail.com>
2024-08-17 20:34:39 +0530

    first commit

---
+++ QuestionandAnswers.py
@@ -0,0 +1,36 @@
+#Q1. Write all the conventions being followed while declaring a variable.
+#1. A variable name must only contain alphabets, digits, and underscore.
+#2. A variable name must start with an alphabet or an underscore only. It cannot start with a digit.
+#3. No white space is allowed within the variable name.
+#4. A variable name must not be any reserved word or keyword
+
+# 2.What will happen if we declare a restricted keyword as a variable?
+#it will throw an error because keywords cannot be used as a varible
+
+#3Can we actually declare a string as a variable name?
+# yes we can declare a string with the varible name
+
+
+#4. Is it possible for us to declare “_” as a variable? If so, then write an example of it.
+# No its not possible to declare "_" as a variable
+
+#Q5. Using an example, explain how the variables in python are dynamic in nature.
+#In Python, variables are dynamically typed, which means that you don’t need to declare the type of a variable explicitly. Instead, the type is determined at runtime based on the value assigned to the variable. This flexibility allows a variable to change its type as needed during execution.
+
+#ex# Initial assignment
+#variable = 10
+# print("Initial value:", variable)
+# print("Type:", type(variable))
+
+# # Changing the type by assigning a different value
+# variable = "Hello, Python!"
+# print("\nAfter changing the value:")
+# print("New value:", variable)
+# print("Type:", type(variable))
+
+# # Further changing the type
+# variable = [1, 2, 3, 4]
+# print("\nAfter changing to a list:")
+# print("New value:", variable)
+# print("Type:", type(variable))
+
